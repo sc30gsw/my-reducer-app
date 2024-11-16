abfcb31a6874e15e4f77eb53999d55779d0c3eb7
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-11-16 23:06:40 +0900

    🚑 簡易的な例

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,26 @@
 import { useReducer } from "react";
 
-const reducer = (state: { count: number }, action: { type: string }) => {
-	switch (action.type) {
-		case "increment":
-			return { ...state, count: state.count + 1 };
-
-		case "decrement":
-			return { ...state, count: state.count - 1 };
-
-		default:
-			throw new Error("不明なactionです");
-	}
-};
-
 function App() {
-	const [state, dispatch] = useReducer(reducer, { count: 0 });
+	const [count, dispatch] = useReducer(
+		(state: number, action: number) => state + action,
+		0,
+	);
 
 	return (
 		<div className="flex flex-col items-center justify-center min-h-dvh w-screen">
-			<p className="text-2xl font-bold">Count: {state.count}</p>
+			<p className="text-2xl font-bold">Count: {count}</p>
 			<div className="flex gap-4 mt-4">
 				<button
 					type="button"
 					className="bg-blue-500 text-white px-4 py-2 rounded-full"
-					onClick={() => dispatch({ type: "increment" })}
+					onClick={() => dispatch(1)}
 				>
 					+
 				</button>
 				<button
 					type="button"
 					className="bg-blue-500 text-white px-4 py-2 rounded-full"
-					onClick={() => dispatch({ type: "decrement" })}
+					onClick={() => dispatch(-1)}
 				>
 					-
 				</button>
