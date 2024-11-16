f02a8cc9917ad5bc2b6207f1278a768c247a6242
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-11-16 23:21:15 +0900

    ✨ 金額編集のサンプル

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { useReducer } from "react";
 
 function App() {
-	const [checked, toggle] = useReducer((state: boolean) => !state, false);
-
+	const [state, dispatch] = useReducer(
+		(_: unknown, action: number) => `${action}円`,
+		"0円",
+	);
 	return (
 		<div className="flex flex-col items-center justify-center min-h-dvh w-screen">
+			<div>{state}</div>
 			<input
-				type="checkbox"
-				className="cursor-pointer w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-				checked={checked}
-				onChange={toggle}
+				type="number"
+				className="block w-3/4 p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500"
+				onChange={(e) => {
+					const { value } = e.target;
+					dispatch(Number(value));
+				}}
 			/>
 		</div>
 	);
