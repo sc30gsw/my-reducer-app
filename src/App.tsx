f503b9d936d2fbb6b37bf1d3674751ad4d171773
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-11-16 23:03:25 +0900

    🚑 基本の形

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
-import { useState } from "react";
+import { useReducer } from "react";
 
-import "./App.css";
+const reducer = (state: { count: number }, action: { type: string }) => {
+	switch (action.type) {
+		case "increment":
+			return { ...state, count: state.count + 1 };
+
+		case "decrement":
+			return { ...state, count: state.count - 1 };
+
+		default:
+			throw new Error("不明なactionです");
+	}
+};
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [state, dispatch] = useReducer(reducer, { count: 0 });
 
-	return <></>;
+	return (
+		<div className="flex flex-col items-center justify-center min-h-dvh w-screen">
+			<p className="text-2xl font-bold">Count: {state.count}</p>
+			<div className="flex gap-4 mt-4">
+				<button
+					type="button"
+					className="bg-blue-500 text-white px-4 py-2 rounded-full"
+					onClick={() => dispatch({ type: "increment" })}
+				>
+					+
+				</button>
+				<button
+					type="button"
+					className="bg-blue-500 text-white px-4 py-2 rounded-full"
+					onClick={() => dispatch({ type: "decrement" })}
+				>
+					-
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
